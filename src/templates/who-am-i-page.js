da6a263e8ed2fa3c74f90241cc61e7b270a52267
Shem <sgyll@protonmail.com>
2019-12-14 03:13:11 +0000

    Index looking good, now adding Who Am I

--- src/templates/who-am-i-page.js
+++ src/templates/who-am-i-page.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const IndexPageTemplate = ({ title, content, contentComponent }) => {
+export const WhoAmIPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
@@ -16,6 +16,7 @@ export const IndexPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
+              {image}
               <PageContent className="content" content={content} />
             </div>
           </div>
@@ -25,38 +26,47 @@ export const IndexPageTemplate = ({ title, content, contentComponent }) => {
   )
 }
 
-IndexPageTemplate.propTypes = {
+WhoAmIPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  image: PropTypes.image,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
-const IndexPage = ({ data }) => {
+const WhoAmIPage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
     <Layout>
-      <IndexPageTemplate
+      <WhoAmIPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        image={post.image}
       />
     </Layout>
   )
 }
 
-IndexPage.propTypes = {
+WhoAmIPage.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-export default IndexPage
+export default WhoAmIPage
 
-export const indexPageQuery = graphql`
-  query IndexPage($id: String!) {
+export const whoAmIPageQuery = graphql`
+  query WhoAmIPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 240, quality: 64) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
